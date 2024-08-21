0745d4198dea48e81b5ae301fb6af097e8904dc4
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-21 16:42:42 +0330

    004 Adding Validation Rules

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,6 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+
 
 @Controller('messages')
 export class MessagesController {
@@ -6,7 +8,7 @@ export class MessagesController {
   listMessages() {}
 
   @Post()
-  createMessage(@Body() body: any) {
+  createMessage(@Body() body: CreateMessageDto) {
     console.log(body);
   }
 
